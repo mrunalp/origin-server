1098bee9d449a66980834ea4eaaa46bf36711b47
Mrunal Patel <mpatel@redhat.com>
2012-08-29 10:33:47 -0700

    Abstract-httpd: Fix usage of old vars.

--- stickshift/abstract/abstract-httpd/info/bin/app_ctl_stop.sh
+++ stickshift/abstract/abstract-httpd/info/bin/app_ctl_stop.sh
@@ -9,12 +9,12 @@ do
     . $f
 done
 
-CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${OPENSHIFT_GEAR_TYPE}/info/configuration/etc/conf
+CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${CARTRIDGE_TYPE}/info/configuration/etc/conf
 
 # Stop the app
 src_user_hook pre_stop_${CARTRIDGE_TYPE}
 set_app_state stopped
-httpd_pid=`cat ${OPENSHIFT_RUN_DIR}httpd.pid 2> /dev/null`
-/usr/sbin/httpd -C "Include ${OPENSHIFT_GEAR_DIR}conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
+httpd_pid=`cat ${OPENSHIFT_HOMEDIR}/${CARTRIDGE_TYPE}/run/httpd.pid 2> /dev/null`
+/usr/sbin/httpd -C "Include ${OPENSHIFT_HOMEDIR}/${CARTRIDGE_TYPE}/conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
 wait_for_stop $httpd_pid
 run_user_hook post_stop_${CARTRIDGE_TYPE}
