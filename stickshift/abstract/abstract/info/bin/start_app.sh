6505023f25fd3bf8b07fdbd3103c3b32ee637838
Mrunal Patel <mpatel@redhat.com>
2012-08-30 16:45:48 -0700

    Fix start_app and stop_app.

--- stickshift/abstract/abstract/info/bin/start_app.sh
+++ stickshift/abstract/abstract/info/bin/start_app.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 echo "Starting application..."
-    for env_var in  ~/.env/*_CTL_SCRIPT
+    for env_var in  $APP_HOME/.env/*_CTL_SCRIPT
     do
         . $env_var
     done
-    for cmd in `awk 'BEGIN { for (a in ENVIRON) if (a ~ /DB_CTL_SCRIPT$/) print ENVIRON[a] }'`
+    for cmd in `awk 'BEGIN {
+                            for (a in ENVIRON)
+                            if (a ~ /DB_CTL_SCRIPT$/)
+                            print ENVIRON[a] }'`
     do
-        $cmd start
+        "$cmd start" || error "Failed to start ${cmd}" 121
     done
-    for cmd in `awk 'BEGIN { for (a in ENVIRON) if ((a ~ /_CTL_SCRIPT$/) && !(a ~ /DB_CTL_SCRIPT$/) && (a != "OPENSHIFT_GEAR_CTL_SCRIPT")) print ENVIRON[a] }'`
+    for cmd in `awk 'BEGIN {
+                            for (a in ENVIRON)
+                                if ((a ~ /_CTL_SCRIPT$/) &&
+                                    !(a ~ /DB_CTL_SCRIPT$/) &&
+                                    !(a ~ /CART_CTL_SCRIPT$/))
+                                        print ENVIRON[a] }'`
     do
-        $cmd start
+        "$cmd start" || error "Failed to start ${cmd}" 121
     done
-    for cmd in `awk 'BEGIN { for (a in ENVIRON) if (a == "OPENSHIFT_GEAR_CTL_SCRIPT") print ENVIRON[a] }'`
+    for cmd in `awk 'BEGIN {
+                        for (a in ENVIRON)
+                            if (a ~ /CART_CTL_SCRIPT$/)
+                                print ENVIRON[a] }'`
     do
-        $cmd start
+        "$cmd start" || error "Failed to start ${cmd}" 121
     done
 echo "Done"
