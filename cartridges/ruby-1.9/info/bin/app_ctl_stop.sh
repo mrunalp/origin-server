d6542c0b13d0687ffebef624cb6b4d9e4c4178a8
Paul Morie <pmorie@gmail.com>
2012-08-31 17:30:26 -0400

    Use namespaced environment variables in ruby-1.9 cart

--- cartridges/ruby-1.9/info/bin/app_ctl_stop.sh
+++ cartridges/ruby-1.9/info/bin/app_ctl_stop.sh
@@ -14,8 +14,8 @@ CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${OPENSHIFT_GEAR_TYPE}/info/configuration/e
 # Stop the app
 src_user_hook pre_stop_${CARTRIDGE_TYPE}
 app_userid=`id -u`
-httpd_pid=`cat ${OPENSHIFT_RUN_DIR}httpd.pid 2> /dev/null`
-/usr/sbin/httpd -C "Include ${OPENSHIFT_GEAR_DIR}conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
+httpd_pid=`cat ${OPENSHIFT_RUBY19_RUN_DIR}/httpd.pid 2> /dev/null`
+/usr/sbin/httpd -C "Include ${OPENSHIFT_RUBY19_CART_DIR}/conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
 for i in {1..20}
 do
     if `ps --pid $httpd_pid > /dev/null 2>&1`  ||  \
