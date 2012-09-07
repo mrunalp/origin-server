ce2a5488b4eb37183a00bdc42f44e126a191d21d
Mrunal Patel <mpatel@redhat.com>
2012-09-06 18:22:05 -0700

    Typeless changes for PHP, MySQL and PhpMyAdmin and changes related to gear script removal in abstract.
    Util wip.
    
    PHP/Abstract changes.
    
    Fix to restart hook.
    
    Take out php ctl script.
    
    Mysql typeless (still need to fix for standalone case and moves)
    
    fix misc vars in php/mysql
    
    phpmyadmin typeless.
    
    Re-enable ns log dirs.
    
    Conflicts:
    	stickshift/abstract/abstract/info/lib/util

--- stickshift/abstract/abstract/info/bin/start_app.sh
+++ stickshift/abstract/abstract/info/bin/start_app.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-echo "Starting application..."
-    for env_var in  ~/.env/*_CTL_SCRIPT
-    do
-        . $env_var
-    done
-    for cmd in `awk 'BEGIN {
-                            for (a in ENVIRON)
-                            if (a ~ /DB_CTL_SCRIPT$/)
-                            print ENVIRON[a] }'`
-    do
-        $cmd start || error "Failed to start ${cmd}" 121
-    done
-    for cmd in `awk 'BEGIN {
-                            for (a in ENVIRON)
-                                if ((a ~ /_CTL_SCRIPT$/) &&
-                                    !(a ~ /DB_CTL_SCRIPT$/) &&
-                                    !(a ~ /CART_CTL_SCRIPT$/))
-                                        print ENVIRON[a] }'`
-    do
-        $cmd start || error "Failed to start ${cmd}" 121
-    done
-    for cmd in `awk 'BEGIN {
-                        for (a in ENVIRON)
-                            if (a ~ /CART_CTL_SCRIPT$/)
-                                print ENVIRON[a] }'`
-    do
-        $cmd start || error "Failed to start ${cmd}" 121
-    done
+source /usr/libexec/stickshift/cartridges/abstract/info/lib/util
+
+start_app
 echo "Done"
