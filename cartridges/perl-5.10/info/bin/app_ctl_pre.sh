33cfbde686fd6d49a5c8c8f01582774a37a0b69a
Dan Mace <ironcladlou@gmail.com>
2012-09-11 09:57:51 -0400

    Perl env refactor

--- cartridges/perl-5.10/info/bin/app_ctl_pre.sh
+++ cartridges/perl-5.10/info/bin/app_ctl_pre.sh
@@ -6,6 +6,9 @@ do
     . $f
 done
 
+cartridge_type="perl-5.10"
+cartridge_dir=$OPENSHIFT_HOMEDIR/$cartridge_type
+
 export REPOLIB="${OPENSHIFT_REPO_DIR}libs/"
-export LOCALSITELIB="${OPENSHIFT_PERL_CART_DIR}perl5lib/lib/perl5/"
+export LOCALSITELIB="${cartridge_dir}/perl5lib/lib/perl5/"
 export PERL5LIB="$REPOLIB:$LOCALSITELIB"
