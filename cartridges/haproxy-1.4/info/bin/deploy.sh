bf98dcb64bcb3cdc04e93841809cb3b5441b3a88
Mrunal Patel <mpatel@redhat.com>
2012-09-17 18:01:36 -0700

    HAProxy WIP.

--- cartridges/haproxy-1.4/info/bin/deploy.sh
+++ cartridges/haproxy-1.4/info/bin/deploy.sh
@@ -11,7 +11,10 @@ source ${CARTRIDGE_BASE_PATH}/abstract/info/lib/util
 
 ${CARTRIDGE_BASE_PATH}/abstract/info/bin/deploy.sh
 
+framework_carts=($(get_installed_framework_carts))
+primary_framework_cart=${framework_carts[0]}
+
 # Sync to the other gears
-${CARTRIDGE_BASE_PATH}/${OPENSHIFT_GEAR_TYPE}/info/bin/sync_gears.sh
+${CARTRIDGE_BASE_PATH}/${primary_framework_cart}/info/bin/sync_gears.sh
 
 #user_deploy.sh
