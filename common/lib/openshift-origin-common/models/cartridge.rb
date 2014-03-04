9abc0ccf44416868ef8adaef85c0bec97601b6d9
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2014-03-04 10:07:05 -0800

    Fix for syntax error in cartridge.rb.
    Change-Id: Ic90d959409114bd573dee3c906a3f9cfaed75aa7

--- common/lib/openshift-origin-common/models/cartridge.rb
+++ common/lib/openshift-origin-common/models/cartridge.rb
@@ -68,7 +68,7 @@ module OpenShift
     end
 
     def global_identifier
-      if self.cartridge_vendor == "redhat" self.cartridge_vendor.to_s.empty?
+      if self.cartridge_vendor == "redhat" || self.cartridge_vendor.to_s.empty?
         short_name
       else
         full_name
